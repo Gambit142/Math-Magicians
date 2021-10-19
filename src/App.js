b28a80e7f05a220a423fa1f2415b8380c51ebd6a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-19 16:41:28 +0100

    Refactor the App component to become a class-based component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import React from 'react';
 import CalculatorComponent from './components/Calculator';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render = () => (
     <CalculatorComponent />
-  );
+  )
 }
 
 export default App;
