ece734b5725bf1794c2bb3af702e2924a2232661
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-20 20:51:50 +0100

    Use state, set state and pass props to children components in the src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import React from 'react';
-import CalculatorComponent from './components/Calculator';
+import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.updateButton = this.updateButton.bind(this);
   }
 
-  render = () => (
-    <CalculatorComponent />
-  )
+  updateButton(currentButton) {
+    const calculatorObject = calculate(this.state, currentButton);
+    this.setState(calculatorObject);
+  }
+
+  render() {
+    return (
+      <Calculator btn={this.updateButton} variable={this.state} />
+    );
+  }
 }
 
 export default App;
