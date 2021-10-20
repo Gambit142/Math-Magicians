cdfb10d446e7b102e5043bde32caa0b7261e825f
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-20 20:48:15 +0100

    Lift state, pass props from App component and create a function that handles events in src/components/Calculator.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Calculator.css';
 
-class CalculatorComponent extends React.Component {
+class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.buttonClicked = this.buttonClicked.bind(this);
   }
 
-  render = () => (
-    <div className="calculator-div">
-      <div className="display-div">0</div>
-      <div className="button-grid">
-        <button type="button">Ac</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="orange-background">&#247;</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="orange-background">&#215;</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="orange-background">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="orange-background">+</button>
-        <button type="button" className="zero">0</button>
-        <button type="button">.</button>
-        <button type="button" className="orange-background">=</button>
+  buttonClicked(e) {
+    const { btn } = this.props;
+    btn(e.target.innerHTML);
+  }
+
+  render() {
+    const { variable } = this.props;
+    const { next, total } = variable;
+    return (
+      <div className="calculator-div">
+        <div className="display-div">{!next ? total : next}</div>
+        <div className="button-grid">
+          <button onClick={this.buttonClicked} type="button">AC</button>
+          <button onClick={this.buttonClicked} type="button">+/-</button>
+          <button onClick={this.buttonClicked} type="button">%</button>
+          <button onClick={this.buttonClicked} type="button" className="orange-background">&#247;</button>
+          <button onClick={this.buttonClicked} type="button">7</button>
+          <button onClick={this.buttonClicked} type="button">8</button>
+          <button onClick={this.buttonClicked} type="button">9</button>
+          <button onClick={this.buttonClicked} type="button" className="orange-background">&#215;</button>
+          <button onClick={this.buttonClicked} type="button">4</button>
+          <button onClick={this.buttonClicked} type="button">5</button>
+          <button onClick={this.buttonClicked} type="button">6</button>
+          <button onClick={this.buttonClicked} type="button" className="orange-background">-</button>
+          <button onClick={this.buttonClicked} type="button">1</button>
+          <button onClick={this.buttonClicked} type="button">2</button>
+          <button onClick={this.buttonClicked} type="button">3</button>
+          <button onClick={this.buttonClicked} type="button" className="orange-background">+</button>
+          <button onClick={this.buttonClicked} type="button" className="zero">0</button>
+          <button onClick={this.buttonClicked} type="button">.</button>
+          <button onClick={this.buttonClicked} type="button" className="orange-background">=</button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default CalculatorComponent;
+Calculator.propTypes = {
+  btn: PropTypes.func.isRequired,
+  variable: PropTypes.shape.isRequired,
+};
+
+export default Calculator;
