7902f05c5bfb2e4a153bd44901eb8b38cbb5d1ef
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-26 23:24:36 +0100

    Create a QuotePage component that will render mathematical quotes in the quote.js file

---
+++ src/components/quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuotePage = () => (
+  <div>
+    <p>
+      Mathematics is not about numbers, equation, computations or
+      algorithms; it is about understanding - William Paul Thurston
+    </p>
+  </div>
+);
+
+export default QuotePage;
