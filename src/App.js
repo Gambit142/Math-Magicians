3e8df478627771e726459975382fc08835ab6656
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-26 23:29:08 +0100

    Update the App component to route paths and make the project a Single Page Application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import calculate from './logic/calculate';
+import NavigationLinks from './links';
+import HomePage from './components/home';
+import QuotePage from './components/quote';
 
 const App = () => {
   const [object, updateCalculatorObject] = useState({
@@ -13,8 +17,22 @@ const App = () => {
     const calculatorObject = calculate(object, currentButton);
     updateCalculatorObject(calculatorObject);
   };
+
   return (
-    <Calculator btn={updateButton} variable={object} />
+    <>
+      <NavigationLinks />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/calculator">
+          <Calculator btn={updateButton} variable={object} />
+        </Route>
+        <Route path="/quotes">
+          <QuotePage />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
